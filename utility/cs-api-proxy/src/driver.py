19f696a9787b1f4d2faa28a60e8049a675e2433b
katzy687 <katzy687@gmail.com>
2022-07-06 00:54:22 -0400

    added sandbox data samples

--- utility/cs-api-proxy/src/driver.py
+++ utility/cs-api-proxy/src/driver.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadResource, \
@@ -6,7 +7,7 @@ from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCom
 #from data_model import *  # run 'shellfoundry generate' to generate data model classes
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 from cloudshell.helpers.sandbox_reporter.reporter import SandboxConsole
-from cloudshell.api.cloudshell_api import SandboxDataKeyValue
+from cloudshell.api.cloudshell_api import SandboxDataKeyValue, SandboxDataKeyValueInfo
 
 
 class CsApiProxyDriver (ResourceDriverInterface):
@@ -64,16 +65,36 @@ class CsApiProxyDriver (ResourceDriverInterface):
         console = SandboxConsole(api, sb_id)
         console.success_print(message)
 
-    def get_sandbox_data(self, context):
+    @staticmethod
+    def _get_sandbox_data(context) -> List[SandboxDataKeyValueInfo]:
         """
         :param ResourceCommandContext context:
         :return:
         """
         api = CloudShellSessionContext(context).get_api()
         sb_id = context.reservation.reservation_id
-        data_keys = api.GetSandboxData(reservationId=sb_id).SandboxDataKeyValues
+        return api.GetSandboxData(reservationId=sb_id).SandboxDataKeyValues
+
+    def get_sandbox_data(self, context):
+        """
+        :param ResourceCommandContext context:
+        :return:
+        """
+        data_keys = self._get_sandbox_data(context)
         dict_data = [{x.Key: x.Value} for x in data_keys]
         return json.dumps(dict_data, indent=4)
+
+    def get_sandbox_data_key(self, context, key):
+        """
+        :param ResourceCommandContext context:
+        :param str key:
+        :return:
+        """
+        data_keys = self._get_sandbox_data(context)
+        key_search = [x.Value for x in data_keys if x.Key == key]
+        if not key_search:
+            raise ValueError(f"Key '{key}' not found in sandbox data")
+        return key_search[0]
 
     def set_sandbox_data(self, context, key, value):
         """
